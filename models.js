28062e6cd02541bca3851ef993c0ba0d88bd973f
Daniel <allprogsandprojects.@gmail.com>
2023-12-11 18:31:21 +0100

    feat: pokus se v tom zorinetovat

--- models.js
+++ models.js
@@ -12,7 +12,6 @@ const UserPasswordModel = mongoose.model("UserPassword", UsersPasswordSchema);
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String,
 });
 
 const UserModel = mongoose.model("User", UserSchema);
